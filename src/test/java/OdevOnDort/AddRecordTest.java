c9bcf4e6dbd6ed28aeed2eee6a5a739223719cd9
edaglaroglu <79571668+edaglaroglu@users.noreply.github.com>
2025-12-14 22:13:40 +0300

    Odev OnDort: CSS Selectors

---
+++ src/test/java/OdevOnDort/AddRecordTest.java
@@ -0,0 +1,102 @@
+package OdevOnDort;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class AddRecordTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //Ready Chrome
+        WebDriverManager.chromedriver().setup();
+        //Create object
+        WebDriver driver = new ChromeDriver();
+        //Go to URL
+        driver.get("https://demoqa.com/webtables");
+        driver.manage().window().maximize();
+
+        //Test: Add record
+
+        WebElement addButton = driver.findElement(By.cssSelector("#addNewRecordButton"));
+        addButton.click();
+        Thread.sleep(2000);
+        WebElement submitButton = driver.findElement(By.cssSelector("#submit"));
+
+        List<WebElement> formFields =
+                driver.findElements(By.cssSelector("form > div > div:nth-of-type(2) input"));
+
+        System.out.println(formFields.size());
+
+        for (WebElement formField : formFields) {
+            String placeHolder = formField.getAttribute("placeholder");
+
+            switch (placeHolder){
+                case "First Name":
+                    formField.sendKeys("Duygu Elif");
+                    break;
+                case "Last Name":
+                    formField.sendKeys("Dağlaroğlu");
+                    break;
+                case "name@example.com":
+                    formField.sendKeys("elif@elif.com");
+                    break;
+                case "Age":
+                    formField.sendKeys("50");
+                    break;
+                case "Salary":
+                    formField.sendKeys("50000");
+                    break;
+                case "Department":
+                    formField.sendKeys("HR");
+                    break;
+            }
+        }
+
+        Thread.sleep(2000);
+        submitButton.click();
+
+        List<WebElement> editIcons =
+                driver.findElements(By.cssSelector("span[title='Edit'] svg"));
+
+        editIcons.get(editIcons.size() - 1).click();
+
+        Thread.sleep(5000);
+
+
+        for (WebElement formField : formFields) {
+            String placeHolder = formField.getAttribute("placeholder");
+
+            switch (placeHolder){
+                case "First Name":
+                    formField.sendKeys("Elif");
+                    break;
+                case "Last Name":
+                    formField.sendKeys("Can");
+                    break;
+                case "name@example.com":
+                    formField.sendKeys("elif@gmail.com");
+                    break;
+                case "Age":
+                    formField.sendKeys("60");
+                    break;
+                case "Salary":
+                    formField.sendKeys("60000");
+                    break;
+                case "Department":
+                    formField.sendKeys("IT");
+                    break;
+            }
+        }
+
+        Thread.sleep(2000);
+        submitButton.click();
+        Thread.sleep(2000);
+        driver.quit();
+
+    }
+}
---
+++ src/test/java/OdevOnDort/ClickButtonTest.java
@@ -0,0 +1,44 @@
+package OdevOnDort;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class ClickButtonTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //Ready Chrome
+        WebDriverManager.chromedriver().setup();
+        //Create object
+        WebDriver driver = new ChromeDriver();
+        //Go to URL
+        driver.get("https://demoqa.com/elements");
+        driver.manage().window().maximize();
+
+        //Test: Click Buttons
+        WebElement buttonsButton = driver.findElement(By.cssSelector("#item-4 > span"));
+        buttonsButton.click();
+        Thread.sleep(5000);
+
+        WebElement clickMeButton = driver.findElement(By.cssSelector("div.mt-4:nth-of-type(3) > button"));
+        clickMeButton.click();
+        Thread.sleep(5000);
+
+        WebElement clickMsg = driver.findElement(By.cssSelector("#dynamicClickMessage"));
+        Assert.assertEquals(clickMsg.getText(), "You have done a dynamic click");
+        Assert.assertTrue(clickMsg.isDisplayed());
+
+        driver.quit();
+
+
+
+
+
+
+
+    }
+}
